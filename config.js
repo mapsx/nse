f7b24fecaa7c1be7de77e72fb3d7e443cb8ded2e
mapsx <143354289+mapsx@users.noreply.github.com>
2026-08-19 09:34:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
 window.NSE_APP_CONFIG = {
-  // IMPORTANT: replace OWNER and REPO after creating your GitHub repository.
-  // Example:
-  // DATA_URL: "https://raw.githubusercontent.com/mapsx/nifty500-nse/main/data/results.json"
-  window.NSE_APP_CONFIG = {
     DATA_URL: "https://raw.githubusercontent.com/mapsx/nse/main/data/results.json"
 };
